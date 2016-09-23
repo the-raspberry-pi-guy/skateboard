e0ac779b8683cfd9ed93cc35b93aca03727d01b5
Matthew Timmons-Brown <theraspberrypiguy@gmail.com>
2016-09-23 21:41:24 +0000

    Update main.py

--- main.py
+++ main.py
@@ -12,9 +12,6 @@ motor = 18
 led = 17
 button = 27
 
-global speed
-speed = 1000
-
 pi.set_PWM_frequency(motor,50)
 pi.set_mode(led,pigpio.OUTPUT)
 pi.set_mode(button,pigpio.INPUT)
@@ -29,12 +26,11 @@ def blinky(times,period):
 
 def connection_process():
         connected = False
-#       pi.set_servo_pulsewidth(18,1000)
         while not connected:
-                blinky(5,0.5)
+                blinky(5,0.4)
                 try:
                         global wii
-                        wii = cwiid.Wiimote(bdaddr=00:1F:C5:86:3E:85)
+                        wii = cwiid.Wiimote(bdaddr="00:1F:C5:86:3E:85")
                         connected = True
                         blinky(40,0.03)
                         wii.rpt_mode = cwiid.RPT_BTN
@@ -47,29 +43,31 @@ def connection_process():
 
 def run_process():
         pi.write(led,1)
-        global speed
+	global speed
         buttons = wii.state['buttons']
 
         if (buttons & cwiid.BTN_B):
-                if speed<=1700:
-                        speed=1350
-                elif speed >= 1700:
-                        for i in range(speed,1350,-1):
+                if speed>=1300:
+                        speed=1500
+                elif speed < 1300:
+                        for i in range(speed,1501,1):
                                 speed=i
+				time.sleep(0.01)
 
-        if (buttons & cwiid.BTN_UP):
-                speed=speed+5
         if (buttons & cwiid.BTN_DOWN):
-                speed=speed-10
+                speed=speed+1
+        if (buttons & cwiid.BTN_UP):
+                speed=speed-1
+	if speed<1000:
+		speed=1000
+	if speed>1720:
+		speed=1720
 
-        #if speed<:
-        #       speed=1000
-        if speed>2050:
-                speed=2000
-        return int(speed)
+	return speed
 
 # Main Program
-
+global speed
+speed=500
 blinky(35,0.05)
 connection_process()
 
