b6a250cea90d22d9e8aab3d9d9eb5b2deaf9f96d
Matthew Timmons-Brown <theraspberrypiguy@gmail.com>
2016-10-22 13:49:47 +0000

    Update skateboard.py with the latest class technology

--- skateboard.py
+++ skateboard.py
@@ -6,26 +6,27 @@ import pigpio
 import time
 import cwiid
 
+pi = pigpio.pi()
+
 class Skateboard:
 	"""An all-powerful skateboard controller"""
-	pi = pigpio.pi()
 	motor = 18
 	led = 17
 	button = 27
 	
 	def __init__(self):
-		pi.set_PWM_frequency(motor,50)
-		pi.set_mode(led,pigpio.OUTPUT)
-		pi.set_mode(button,pigpio.INPUT)
-		pi.set_pull_up_down(button, pigpio.PUD_UP)
+		pi.set_PWM_frequency(self.motor,50)
+		pi.set_mode(self.led,pigpio.OUTPUT)
+		pi.set_mode(self.button,pigpio.INPUT)
+		pi.set_pull_up_down(self.button, pigpio.PUD_UP)
 		self.speed=1500
 
 	def blinky(self,times,period):
-		for i in range (1,self.times):
-			pi.write(led,1)
-			time.sleep(self.period)
-			pi.write(led,0)
-			time.sleep(self.period)
+		for i in range (1,times):
+			pi.write(self.led,1)
+			time.sleep(period)
+			pi.write(self.led,0)
+			time.sleep(period)
 
 	def connection_process(self):
 		connected = False
@@ -35,17 +36,16 @@ class Skateboard:
 				self.wii = cwiid.Wiimote(bdaddr="00:1F:C5:86:3E:85")
 				connected = True
 				self.blinky(40,0.03)
-				wii.rpt_mode = cwiid.RPT_BTN
-				wii.rumble = 1
+				self.wii.rpt_mode = cwiid.RPT_BTN
+				self.wii.rumble = 1
 				time.sleep(1)
-				wii.rumble = 0
-				return wii
+				self.wii.rumble = 0
 			except RuntimeError:
 				pass
 
 	def run_process(self):
-		pi.write(led,1)
-		self.buttons = wii.state['buttons']
+		pi.write(self.led,1)
+		self.buttons = self.wii.state['buttons']
 
 		if (self.buttons & cwiid.BTN_B):
 			if self.speed>=1300:
@@ -67,7 +67,9 @@ class Skateboard:
 ### Main Program ###
 
 skate = Skateboard()
-skate.blinky(35,0.05)
+#skate.blinky(35,0.05)
 skate.connection_process()
 while True:
+	print(skate.speed)
+	skate.run_process()
 	pi.set_servo_pulsewidth(18,skate.speed)
